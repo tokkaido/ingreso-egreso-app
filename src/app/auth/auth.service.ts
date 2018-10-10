cb2a60df52aa35e4d7f53a9e998aae8bf555cdc8
Hector Sandoval <tokkaido@hotmail.com>
2018-10-10 15:12:49 -0300

    seccion 8 lista - inicio redux

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -10,11 +10,15 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { Store } from '@ngrx/store';
 import { AppState } from '../app.reducer';
 import { ActivarLoadingAction, DesactivarLoadingAction } from '../shared/ui.actions';
+import { SetUserAction } from './auth.actions';
+import { Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  private userSubscription: Subscription = new Subscription();
 
   constructor(
     private afAuth: AngularFireAuth,
@@ -26,7 +30,16 @@ export class AuthService {
   initAuthListener() {
     this.afAuth.authState.subscribe( (fbUser: firebase.User) => {
       // console.log(fbUser);
-
+      if ( fbUser ) {
+        this.userSubscription = this.afDB.doc(`${ fbUser.uid }/usuario`).valueChanges()
+        .subscribe( (userObj: any) => {
+          const newUser = new User( userObj );
+          console.log( newUser );
+          this.store.dispatch( new SetUserAction( newUser) );
+        });
+      } else {
+        this.userSubscription.unsubscribe();
+      }
     });
   }
 
